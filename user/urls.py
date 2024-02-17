e0e93fc8b6e18bfa176cbab2f95b66348a109b2b
hafiyhakimi <hafiyhakimi1665@gmail.com>
2024-02-18 03:24:40 +0800

    User module in progress & Updated the static files

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
+from django.urls import re_path as url, include
 from . import views
+from .views import *
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 app_name = 'user'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
     # Define other URL patterns here
 ]
+
+urlpatterns += staticfiles_urlpatterns()
